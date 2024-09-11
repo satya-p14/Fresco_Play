f4e8728cfafd23a405cb8d27983e10c370370499
satya-p14 <vikkisatya@gmail.com>
2024-09-12 01:00:33 +0530

    Create promise.js

---
+++ Nodejs / Serenade/promise.js
@@ -0,0 +1,12 @@
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+fs.readFileAsync("users.json", "utf8").then(function (usersResp) {
+  console.log(usersResp);
+}).catch(function (error) {
+  console.error(error.stack);
+});
+
+
+// help link 
+
+https://github.com/satya-p14/Fresco_Play/tree/main/Nodejs-promises-handson
