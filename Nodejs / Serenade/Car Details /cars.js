4cb9f1ebd1479d436a4a65b0e5ecede2dd8f4b07
satya-p14 <vikkisatya@gmail.com>
2024-09-12 01:24:31 +0530

    Update cars.js

--- Nodejs / Serenade/Car Details /cars.js
+++ Nodejs / Serenade/Car Details /cars.js
@@ -2,6 +2,18 @@ const express = require("express");
 //setting up the cars router
 const carsRouter = express.Router();
 //write your code for the endpoints here
+carsRouter.post("/cars", async (req, res) => {
+  const carObj = {
+    _id : new mongoose.Types.ObjectId(),
+    name: 'New Car',
+    price: 2040000,
+    capacity: 5,
+    type: 'Sedan',
+    manufacturer: 'Hundai'
+  };
+  await cars.save()
+  res.status(201).send(cars);
+})
 
 //exporting the router
 module.exports = carsRouter;
