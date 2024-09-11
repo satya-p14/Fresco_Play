e41c31bc1468c18a58197b8566ab82aa7b7fb937
satya-p14 <vikkisatya@gmail.com>
2024-09-12 01:10:12 +0530

    Create cars.js

---
+++ Nodejs / Serenade/Car Details /cars.js
@@ -0,0 +1,7 @@
+const express = require("express");
+//setting up the cars router
+const carsRouter = express.Router();
+//write your code for the endpoints here
+
+//exporting the router
+module.exports = carsRouter;
