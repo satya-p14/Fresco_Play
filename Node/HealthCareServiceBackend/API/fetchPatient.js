1bc4dcdda014efad32a260ddc5da23500d50c251
satya-p14 <vikkisatya@gmail.com>
2024-09-12 00:02:54 +0530

    Create fetchPatient.js

---
+++ Node/HealthCareServiceBackend/API/fetchPatient.js
@@ -0,0 +1,15 @@
+let patientDetails = require('../models/patients');
+let mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+let jwt = require('jsonwebtoken');
+let config = require('../config');
+const mongo = require('mongodb').MongoClient;
+
+module.exports = function (req, res) {
+  /*write your code here*/
+  Patient.find((err, patients) => {
+    if (err)
+      return res.status(401).json(err);
+    return res.json(patients);
+  })
+}
